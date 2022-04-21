ff57a89285cbbee3a41b4b274981aed827ea6575
Keisuke Harada <harada@entap.co.jp>
2022-04-21 20:12:43 +0900

    記事詳細画面表示（showアクションの追加）

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/articles', [ArticleController::class, 'index'])->name('articles.ind
 Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
 
 Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
+
+Route::get('/articles/{article}',[ArticleController::class,'show'])->name('articles.show');
+
